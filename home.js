8fb4c1e6713d48ca74a834dd90d0e5c9a94ead3a
devfernando <fernandoamev@gmail.com>
2025-11-07 04:31:34 -0300

    Nova home finalizada

--- home.js
+++ home.js
@@ -1,3 +1,9 @@
 function mostraNome(nome) {
     return nome;
 }
+
+function mostraNome(sobrenome) {
+    return sobrenome;
+}
+
+//finalizando o sistema da home
--- login.js
+++ login.js
@@ -1,3 +1,11 @@
 //Trabalhando no login
 
 //Finalizando o sistema de login
+
+//Fazendo alterações no login
+function login (nome, email) {
+    return 'Bem vindo'
+}
+
+//sistema de login completo
+//retornando  o nome do usuário
